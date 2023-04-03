05109df6392bb1b2dee100d1466964cf78b0a9de
hanifrev <hanifrev@gmail.com>
2023-04-03 20:11:35 +0700

    accordion

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,23 +4,27 @@ import "react-datepicker/dist/react-datepicker.css";
 import Image from "next/image";
 import clock from "../assets/clock.svg";
 import description from "../assets/description.svg";
+import expand from "../assets/expand.svg";
 import moment from "moment";
 
 const Todo = ({ addTask }) => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
   const [dueDate, setDueDate] = useState(null);
+  const [desc, setDesc] = useState("");
   const [theForm, setTheForm] = useState(false);
+  const [accordion, setAccordion] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     setTodos([
       ...todos,
-      { text: inputValue, completed: false, dueDate, description: "" },
+      { text: inputValue, completed: false, dueDate, description: desc },
     ]);
     setInputValue("");
     setDueDate(null);
+    setDesc("");
     setTheForm(false);
   };
 
@@ -72,6 +76,19 @@ const Todo = ({ addTask }) => {
               className="bg-transparent datePick text-black"
             />
           </div>
+          <div className="flex flex-row pl-9 pt-3">
+            <Image
+              src={description}
+              alt="img"
+              className="w-[15px] mr-[18px] -mt-[17px]"
+            />
+            <textarea
+              value={desc}
+              placeholder="No Description"
+              onChange={(e) => setDesc(e.target.value)}
+              className="bg-transparent textarea text-black"
+            />
+          </div>
           <div className="flex flex-row gap-4 justify-end my-4 cta">
             <button onClick={() => setTheForm(false)}>Cancel</button>
             <button>Add</button>
@@ -81,7 +98,11 @@ const Todo = ({ addTask }) => {
 
       {todos.map((todo, index) => (
         <div key={index} className="todo-item text-black flex flex-col">
-          <div className="flex flex-row justify-between pb-3">
+          <div
+            className={`flex flex-row justify-between ${
+              accordion ? "pb-3" : "pb-[19.5px]"
+            }`}
+          >
             <label className="flex flex-row ">
               <input
                 type="checkbox"
@@ -98,32 +119,43 @@ const Todo = ({ addTask }) => {
               <div className="text-sm mr-[10px] mt-1">
                 {moment(todo.dueDate).format("MM/DD/YYYY")}
               </div>
-              <div className="mr-[10px]">v</div>
+              <div
+                className={`mr-[10px] pt-[5px] ${
+                  accordion ? "transform rotate-180" : ""
+                }`}
+                onClick={() => setAccordion(!accordion)}
+              >
+                <Image src={expand} alt="img" />
+              </div>
               <div onClick={() => deleteTodo(index)}>Delete</div>
             </div>
           </div>
 
-          <div className="flex flex-row pl-9">
-            <Image src={clock} alt="img" className="w-[20px] mr-[18px]" />
-            <DatePicker
-              selected={todo.dueDate}
-              onChange={(date) => handleCalendar(date, index)}
-              className="bg-transparent datePick"
-            />
-          </div>
-          <div className="flex flex-row pl-9 pt-[13px]">
-            <Image
-              src={description}
-              alt="img"
-              className="w-[15px] mr-[18px] -mt-5"
-            />
-            <textarea
-              value={todo.description}
-              placeholder="No Description"
-              onChange={(e) => handleDescription(e, index)}
-              className="bg-transparent"
-            />
-          </div>
+          {accordion && (
+            <div className="accordion" key={index}>
+              <div className="flex flex-row pl-9">
+                <Image src={clock} alt="img" className="w-[20px] mr-[18px]" />
+                <DatePicker
+                  selected={todo.dueDate}
+                  onChange={(date) => handleCalendar(date, index)}
+                  className="bg-transparent datePick"
+                />
+              </div>
+              <div className="flex flex-row pl-9 pt-[13px]">
+                <Image
+                  src={description}
+                  alt="img"
+                  className="w-[15px] mr-[18px] -mt-5"
+                />
+                <textarea
+                  value={todo.description}
+                  placeholder="No Description"
+                  onChange={(e) => handleDescription(e, index)}
+                  className="bg-transparent p-[15px] ml-1"
+                />
+              </div>
+            </div>
+          )}
         </div>
       ))}
     </div>
